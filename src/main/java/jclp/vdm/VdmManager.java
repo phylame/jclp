1ae81e4fb96de473ea9e53c536e4d7fd52096e34
wp <phylame@163.com>
2017-08-10 17:30:13 +0800

    change vdm parameter to Object

--- src/main/java/jclp/vdm/VdmManager.java
+++ src/main/java/jclp/vdm/VdmManager.java
@@ -12,12 +12,12 @@ public final class VdmManager extends ServiceManager<VdmFactory> {
         super(VdmFactory.class);
     }
 
-    public static VdmReader getReader(String name, String input) throws IOException {
+    public static VdmReader getReader(String name, Object input) throws IOException {
         val factory = instance.getService(name);
         return factory != null ? factory.getReader(input) : null;
     }
 
-    public static VdmWriter getWriter(String name, String output) throws IOException {
+    public static VdmWriter getWriter(String name, Object output) throws IOException {
         val factory = instance.getService(name);
         return factory != null ? factory.getWriter(output) : null;
     }
